38048b3964758473a8f8ed02186a7ac891cae1df
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-09-28 13:19:29 +0900

    more acculate index

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
 cat <<EOF
  'EOF' | mysql -h ${myhost} -P ${myport} -u ${myuser} -e
     CREATE INDEX user_idx_login ON users (login);
-    CREATE INDEX login_log_idx_id_user_id_succeeded ON login_log (id, user_id, succeeded);
-    CREATE INDEX login_log_idx_id_user_id_ip ON login_log (id, user_id, ip);
-    CREATE INDEX login_log_idx_ip_id ON login_log (ip, id);
+    CREATE INDEX login_log_idx_user_id_succeeded_id ON login_log (user_id, succeeded, id);
+    CREATE INDEX login_log_idx_user_id_ip_id ON login_log (user_id, ip, id);
+    CREATE INDEX login_log_idx_ip_user_id ON login_log (ip, user_id);
 EOF
