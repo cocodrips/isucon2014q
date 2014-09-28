79d2027d0809d969310cddd28ad9257b42b5482f
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-09-28 13:33:57 +0900

    add idx_user_id_ip_id

--- init.sh
+++ init.sh
@@ -17,5 +17,6 @@ cat <<EOF
     CREATE INDEX user_idx_login ON users (login);
     CREATE INDEX login_log_idx_user_id_succeeded_id ON login_log (user_id, succeeded, id);
     CREATE INDEX login_log_idx_user_id_ip_id ON login_log (user_id, ip, id);
+    CREATE INDEX login_log_idx_user_id_ip_id ON login_log (succeeded, ip, id);
     CREATE INDEX login_log_idx_ip_user_id ON login_log (ip, user_id);
 EOF
